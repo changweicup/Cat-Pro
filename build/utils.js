3b3bb2ad3937179ba129e9f8f69572f53ab0b037
张长伟 <changwei.zhang@hand-china.com>
2020-04-22 16:47:16 +0800

    [ADD]React整合

--- build/utils.js
+++ build/utils.js
@@ -3,3 +3,4 @@ const path = require("path")
 exports.resolve = function (dir) {
     return path.resolve(__dirname, dir)
 }
+
--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('hello');
+import React from 'react'
+import ReactDom from 'react-dom'
+import HomePage from "./home"
+class App extends React.Component {
+    render(){
+        return (
+            <div>
+                <HomePage />
+            </div>
+        )
+    }
+}
+ReactDom.render(<App/>,document.getElementById("app"))
