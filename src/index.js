723b7bdb58167f684f40714e8b8b5237aa38deb2
张长伟 <changwei.zhang@hand-china.com>
2020-04-24 15:16:40 +0800

    [ADD]mobx使用

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import Index from './router/Index'
+import { Provider } from "mobx-react"
+import store from "./router/store"
 import './app.less'
 
 class App extends React.Component {
     render(){
         return (
             <div>
-                <Index />
+                <Provider {...store}>
+                    <Index />
+                </Provider>
             </div>
         )
     }
 }
-ReactDom.render(<App/>,document.getElementById("app"))
+ReactDom.render(<App/>,document.getElementById("app"));
